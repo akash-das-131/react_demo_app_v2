67f6a12b55c89c0946b6a5c43fe4c31185ba4ea7
Rohit_CK <rohit@cloudkaptan.com>
2022-10-16 12:09:54 +0530

    start react project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Start Project
+          Start Project react
         </a>
       </header>
     </div>
